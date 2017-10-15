19b90ebdcaf54da81a2f25b78595f5bcaccf4b0f
David Prieto <dprietoc@gmail.com>
2017-10-15 12:32:57 -0300

    AWS DB configs

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	url: 'mongodb://dprieto:dprieto987@ec2-18-231-71-128.sa-east-1.compute.amazonaws.com:27017/dummyDB'
+	
 }
